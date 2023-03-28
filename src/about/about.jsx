bfd9f21728ac8054d850704662d03e189937bb90
Lee <lee.jensen@agilix.com>
2023-03-28 09:20:27 -0600

    fix(react) removed class style component.

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './about.css';
 
-// Demonstrates using React class style component
-export class About extends React.Component {
-  constructor(props) {
-    super(props);
+export function About(props) {
+  const [imageUrl, setImageUrl] = React.useState('');
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
 
-    this.state = {
-      imageUrl: '',
-      quote: '',
-      quoteAuthor: '',
-    };
-
+  // We only want this to render the first time the component is created and so we provide an empty dependency list.
+  React.useEffect(() => {
     const random = Math.floor(Math.random() * 1000);
     fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
       .then((response) => response.json())
@@ -20,57 +16,48 @@ export class About extends React.Component {
 
         const width = containerEl.offsetWidth;
         const height = containerEl.offsetHeight;
-        const imageUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
-        this.setState({ imageUrl: imageUrl });
+        const apiUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        setImageUrl(apiUrl);
       })
-      .catch(() => {
-        // Use offline fallback and don't display an image
-      });
+      .catch();
 
     fetch('https://api.quotable.io/random')
       .then((response) => response.json())
       .then((data) => {
-        this.setState({ quote: data.content, quoteAuthor: data.author });
+        setQuote(data.content);
+        setQuoteAuthor(data.author);
       })
-      .catch(() => {
-        // Use offline fallback classic quote
-        this.setState({
-          quote: `Always bet on JavaScript`,
-          quoteAuthor: `Brendan Eich`,
-        });
-      });
+      .catch();
+  }, []);
+
+  let imgEl = '';
+
+  if (imageUrl) {
+    imgEl = <img src={imageUrl} alt='stock background' />;
   }
 
-  render() {
-    let imgEl = '';
-
-    if (this.state.imageUrl) {
-      imgEl = <img src={this.state.imageUrl} alt='pretty' />;
-    }
-
-    return (
-      <main className='container-fluid bg-secondary text-center'>
-        <div>
-          <div id='picture' className='picture-box'>
-            {imgEl}
-          </div>
-
-          <p>
-            Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake.
-            The longer the sequence you repeat, the greater your score.
-          </p>
-
-          <p>
-            The name Simon is a registered trademark of Milton-Bradley. Our use of the name and the game is for
-            non-profit educational use only. No part of this code or program should be used outside of that definition.
-          </p>
-
-          <div className='quote-box bg-light text-dark'>
-            <p className='quote'>{this.state.quote}</p>
-            {this.state.quoteAuthor && <p className='author'>{this.state.quoteAuthor}</p>}
-          </div>
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        <div id='picture' className='picture-box'>
+          {imgEl}
         </div>
-      </main>
-    );
-  }
+
+        <p>
+          Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake.
+          The longer the sequence you repeat, the greater your score.
+        </p>
+
+        <p>
+          The name Simon is a registered trademark of Milton-Bradley. Our use of the name and the game is for non-profit
+          educational use only. No part of this code or application may be used outside of that definition.
+        </p>
+
+        <div className='quote-box bg-light text-dark'>
+          <p className='quote'>{quote}</p>
+          <p className='author'>{quoteAuthor}</p>
+        </div>
+      </div>
+    </main>
+  );
 }
